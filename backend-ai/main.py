bbc9bc941916b5469ed1c0c2ae80e71464743910
Tobias Nobile <tobias.nobile@hotmail.com>
2026-02-28 11:07:28 +0100

    Image classifiction : add edge density calculating

--- backend-ai/main.py
+++ backend-ai/main.py
@@ -4,6 +4,8 @@ import numpy as np
 def laplacian_variance(img_gray):
     return cv2.Laplacian(img_gray, cv2.CV_64F).var()
 
-
+def edge_density(img_gray):
+    edges = cv2.Canny(img_gray, 100, 200)
+    return np.sum(edges > 0) / edges.size
 
 

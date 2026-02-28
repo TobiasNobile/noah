f9f200296206697ca8df242103a1f9631a71cbf9
Tobias Nobile <tobias.nobile@hotmail.com>
2026-02-28 11:36:41 +0100

    Backend and assets : add 2 images to test metrics ; generating outputes

--- backend-ai/main.py
+++ backend-ai/main.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 from core.classifier import classify_image
 
-ROOT_DIR = Path(__file__).resolve().parent.parent.parent
-ASSETS_DIR = ROOT_DIR / "assets"
+ROOT_DIR = Path(__file__).resolve().parent.parent
+ASSETS_DIR = ROOT_DIR / "assets" / "images"
 
 results = []
 for path in ASSETS_DIR.glob("*.jpg"):
     results.append(classify_image(path))
 
-with open("metrics.json", "w") as f:
+with open("assets/outputs/metrics.json", "w") as f:
     json.dump(results, f, indent=2)
 
 print(f"{len(results)} classified images")

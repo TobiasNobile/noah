3942b245e8b4435a55872caceae64a443d485f7c
Théo <theo.audurier@gmail.com>
2026-02-28 12:44:29 +0100

    Added tracking class.

--- mobile-app/composeApp/src/androidMain/kotlin/com/hackathonteam/noah/tracking/TrackingManager.kt
+++ mobile-app/composeApp/src/androidMain/kotlin/com/hackathonteam/noah/tracking/TrackingManager.kt
@@ -2,21 +2,21 @@ package com.hackathonteam.noah.tracking
 
 class TrackingManager {
 
-    private var instance: TrackingManager ? = null;
-    private var state: TrackingState = TrackingState.IDLE;
+    private var instance: TrackingManager ? = null
+    private var state: TrackingState = TrackingState.IDLE
 
     fun getInstance(): TrackingManager {
         if(instance == null){
-            instance = TrackingManager();
+            instance = TrackingManager()
         }
-        return instance!!;
+        return instance!!
     }
 
     fun getState(): TrackingState {
-        return state;
+        return state
     }
 
     fun setState(state: TrackingState) {
-        this.state = state;
+        this.state = state
     }
 }

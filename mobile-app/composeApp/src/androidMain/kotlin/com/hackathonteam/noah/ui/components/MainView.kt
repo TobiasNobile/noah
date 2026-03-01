68ece6b4d9f8d1ea7dd4055a860d097b545d5406
Théo <theo.audurier@gmail.com>
2026-03-01 19:10:13 +0100

    Removed unuseful files

--- mobile-app/composeApp/src/androidMain/kotlin/com/hackathonteam/noah/ui/components/MainView.kt
+++ mobile-app/composeApp/src/androidMain/kotlin/com/hackathonteam/noah/ui/components/MainView.kt
@@ -8,13 +8,11 @@ import android.os.Vibrator
 import android.os.VibratorManager
 import android.speech.tts.TextToSpeech
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Stop
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
